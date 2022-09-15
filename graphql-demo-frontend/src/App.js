016eed6083e793ee09f88115f9db9711994b198a
Roman Matveev <r.matveev@socrat.marketing>
2022-09-15 14:58:45 +1000

    15.09.2022 -- Добавлен каталог с React фронтендом. Добавление компонентов. Часть 1.

--- graphql-demo-frontend/src/App.js
+++ graphql-demo-frontend/src/App.js
@@ -1,24 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import {SecureRoute} from "@okta/okta-react";
+
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from "./Components/Home";
+import IssueTracker from "./Components/IssueTracker";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Router>
+        <Route path='/' exact={true} component={Home} />
+        <SecureRoute path='/issue-tracker' component={IssueTracker} />
+      </Router>
   );
 }
 
--- graphql-demo-frontend/src/index.js
+++ graphql-demo-frontend/src/index.js
@@ -3,12 +3,38 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {ApolloClient, ApolloProvider, createHttpLink, from, InMemoryCache} from "@apollo/client";
+import {setContext} from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:1010/graphql',
+});
+
+const oktaTokenStorage = JSON.parse(localStorage.getItem('okta-token-storage'));
+const accessToken = oktaTokenStorage?.accessToken?.accessToken;
+
+// inject the access token into the Apollo Client
+const authLink = setContext((_, { headers }) => {
+    const token = accessToken;
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
+
+const client = new ApolloClient({
+    cache: new InMemoryCache(),
+    link: from([authLink, httpLink])
+});
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+       <App />
+    </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
